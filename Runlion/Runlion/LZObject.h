6ea5ee6ae4151bfd9500c72ceabf99f119043afd
runlion <runlion_app@runlion.com>
2017-12-07 14:31:54 +0800

    no message

---
+++ Runlion/Runlion/LZObject.h
@@ -0,0 +1,13 @@
+//
+//  LZObject.h
+//  Runlion
+//
+//  Created by ff on 2017/12/7.
+//  Copyright © 2017年 ff. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LZObject : NSObject
+
+@end
---
+++ Runlion/Runlion/LZObject.m
@@ -0,0 +1,13 @@
+//
+//  LZObject.m
+//  Runlion
+//
+//  Created by ff on 2017/12/7.
+//  Copyright © 2017年 ff. All rights reserved.
+//
+
+#import "LZObject.h"
+
+@implementation LZObject
+
+@end
